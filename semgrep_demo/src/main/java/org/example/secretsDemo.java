18b07d3ba830730d56a9cc05d9ae524d4e8d1352
devsecopst <devsecopst@gmail.com>
2025-05-30 21:10:30 +0300

    new changes

--- semgrep_demo/src/main/java/org/example/secretsDemo.java
+++ semgrep_demo/src/main/java/org/example/secretsDemo.java
@@ -8,9 +8,12 @@ public class secretsDemo {
 
         // 🔐 GitHub Token
         String githubToken = "ghp_D8fR2sY9ABQpGgW2S3xF4dVYZkNpWqz57TbR";
-
+        String githubToken44 = "ghp_D8fR2sY9ABQpGgW2S3xF4dVYZkNpWqz57TbR";
         // 🔐 GitLab Token
         String gitlabToken = "glpat-28eFqsNLmXKJxuPbaYBZ";
+
+
+
 
 
         String gitlabToken2 = "glpat-28eFqsNLmXKJxuPbaYBZ";
