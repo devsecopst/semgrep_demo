9e7b97eb964e609851b7f46c3346c1e38d5d9d4d
devsecopst <devsecopst@gmail.com>
2025-05-30 19:01:29 +0300

    test

--- semgrep_demo/src/main/java/org/example/secretsDemo.java
+++ semgrep_demo/src/main/java/org/example/secretsDemo.java
@@ -20,7 +20,6 @@ public class secretsDemo {
         // 🔐 Private RSA key
         String privateKey = "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASC...\n-----END PRIVATE KEY-----";
 
-
         // 🧬 GitHub token
         String githubToken = "ghp_1234567890abcdefghijklmnopqrstuvwx";
         // ❌ Embedded credentials in URL
