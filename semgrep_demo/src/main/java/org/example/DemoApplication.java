420b928273881ea0637fbe08cc6e0c1f9e831868
devsecopst <devsecopst@gmail.com>
2025-06-23 16:01:11 +0300

    test

--- semgrep_demo/src/main/java/org/example/DemoApplication.java
+++ semgrep_demo/src/main/java/org/example/DemoApplication.java
@@ -6,6 +6,7 @@ public class DemoApplication {
         // SQL Injection
         service.vulnerableSqlQuery("admin' OR '1'='1");
 
+
         // Print the hardcoded secret
         service.printApiKey();
         // Jackson insecure deserialization demo payload (crafted malicious JSON)
