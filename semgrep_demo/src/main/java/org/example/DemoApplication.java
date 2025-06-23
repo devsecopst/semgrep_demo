a0e543081c6630bc68675262905abf8cada190f4
devsecopst <devsecopst@gmail.com>
2025-06-23 16:46:07 +0300

    test

--- semgrep_demo/src/main/java/org/example/DemoApplication.java
+++ semgrep_demo/src/main/java/org/example/DemoApplication.java
@@ -6,6 +6,7 @@ public class DemoApplication {
         // SQL Injection
         service.vulnerableSqlQuery("admin' OR '1'='1");
 
+        String jwtSecret = "P8Lm9uQ93YzFz6Ar@#5cLKMnf8!JHbdE6vZ4UPpTfL";
 
         // Print the hardcoded secret
         service.printApiKey();
