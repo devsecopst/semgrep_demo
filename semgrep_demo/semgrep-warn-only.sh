68f0984ff1f70e3e872ad885d0ed9be86daa1c3e
devsecopst <devsecopst@gmail.com>
2025-07-06 02:12:40 +0300

    Test

--- semgrep_demo/semgrep-warn-only.sh
+++ semgrep_demo/semgrep-warn-only.sh
@@ -18,6 +18,6 @@ fi
 echo "🔍 Running Semgrep scan (warn-only)..."
 #one time execution.
 semgrep login 1>/dev/null
-semgrep ci --code --secrets --includes $staged_Files --dry-run 2>/dev/null
+semgrep ci --code --secrets --include $staged_Files --dry-run 2>/dev/null
 echo -e "\n${GREEN}✔️Semgrep security code scan completed.\n\r${NC} ${RED}📄 For any questions or concerns, please contact the Security Team.${NC}"
 exit 0
