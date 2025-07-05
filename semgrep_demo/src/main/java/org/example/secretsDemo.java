216233e3a5ae94a8dd2a4096414966e78795c558
devsecopst <devsecopst@gmail.com>
2025-07-05 20:49:30 +0300

    test

--- semgrep_demo/src/main/java/org/example/secretsDemo.java
+++ semgrep_demo/src/main/java/org/example/secretsDemo.java
@@ -83,7 +83,7 @@ public class secretsDemo {
         // 🚨 Hardcoded JWT with real-looking structure
         String jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
                 + "eyJ1c2VyX2lkIjoxMjM0LCJyb2xlIjoiYWRtaW4ifQ."
-                + "f4zH_Mv_5uJazJxJp81UvZxPQVqDq2RjY3MHe-L1o";
+                + "f4zH_Mv_5uJazJxJp8243241UvZxPQVqDq2RjY3MHe-L1o";
         // Output
         System.out.println("Simulated secrets for security testing. DO NOT deploy to production.");
     }
