f81a855c9ecd427a4a342f9cd1a5e882d1a9ad1e
devsecopst <devsecopst@gmail.com>
2025-07-05 20:32:22 +0300

    test:

--- semgrep_demo/semgrep-warn-only.sh
+++ semgrep_demo/semgrep-warn-only.sh
@@ -2,11 +2,20 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
+SEMGREP_REPO_URL=local://my-local-project
+#get Staged files.
+staged_Files=$(git diff --cached  --name-only --diff-filter=ACM)
+echo -e "scan against:$staged_Files"
+
+#do we have staged files or not?
+if [ -z "$staged_Files" ]; then
+  echo "staged_Files is null or empty. Exiting with status 0."
+  exit 0
+fi
 
 echo "🔍 Running Semgrep scan (warn-only)..."
-semgrep login 1>/dev/null
-semgrep ci --code --secrets --include $(git diff --name-only --cached) --dry-run 2>/dev/null
+#one time execution.
+#semgrep login 2>/dev/null
+semgrep ci --code --secrets --include $staged_Files --dry-run 2>/dev/null
 echo -e "\n${GREEN}✔️Semgrep security code scan completed.\n\r${NC} ${RED}📄 For any questions or concerns, please contact the Security Team.${NC}"
 exit 0
-
-
