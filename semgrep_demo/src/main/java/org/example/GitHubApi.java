909c8a939acfbc654156f14c9647eff6e54bba01
devsecopst <devsecopst@gmail.com>
2025-07-15 23:09:23 +0300

    test

--- semgrep_demo/src/main/java/org/example/GitHubApi.java
+++ semgrep_demo/src/main/java/org/example/GitHubApi.java
@@ -11,6 +11,7 @@ public class GitHubApi {
 
 
 
+
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Authorization", "token " + token);
