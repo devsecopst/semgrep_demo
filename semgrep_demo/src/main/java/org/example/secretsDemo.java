31be427c0d3549fcfd0ee5d9d397eca1d6963fab
devsecopst <devsecopst@gmail.com>
2025-07-30 22:36:25 +0300

    test

--- semgrep_demo/src/main/java/org/example/secretsDemo.java
+++ semgrep_demo/src/main/java/org/example/secretsDemo.java
@@ -21,6 +21,7 @@ public class secretsDemo {
         String twilioAuthToken4 = "a7f4a9bc3dcf7e8f230ff7ab6f0e2d13";
         // 🧪 Firebase Server Key
 
+
         String firebaseServerKey = "AAAAjKo9EXw:APA91bGT8PX5g7Vl3m_3hwPnF03mZqFik_P8F9xdm3V";
         // 🔐 Shopify Private App Password
         String shopifyAppPassword = "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";
