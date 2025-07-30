6c61eec01032eab6ccf0790fb1fe740f89f2dc17
devsecopst <devsecopst@gmail.com>
2025-07-30 22:51:08 +0300

    test

--- semgrep_demo/src/main/java/org/example/GitHubApi.java
+++ semgrep_demo/src/main/java/org/example/GitHubApi.java
@@ -8,20 +8,9 @@ public class GitHubApi {
         // BAD: Hardcoded GitHub personal access token
         String token = "ghp_1A$5435342b3C4d5E6f7G8h9I0jKlMnOpQrStUvWxYz";
         URL url = new URL("https://api.github.com/user/repos");
-
-
-
-
-
-
-
-
-
-
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Authorization", "token " + token);
-
         int responseCode = conn.getResponseCode();
         System.out.println("Response Code: " + responseCode);
     }
