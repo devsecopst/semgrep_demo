cb502712fc0568e97de1d847ac8236da485f6b64
devsecopst <devsecopst@gmail.com>
2025-08-10 01:23:51 +0300

    test

--- semgrep_demo/src/main/java/org/example/PureVulnerable.java
+++ semgrep_demo/src/main/java/org/example/PureVulnerable.java
@@ -36,6 +36,7 @@ public class PureVulnerable {
             }
 
 
+
         } else {
             System.out.println("File  not found.");
         }
--- semgrep_demo/src/main/java/org/example/RequestMapping.java
+++ semgrep_demo/src/main/java/org/example/RequestMapping.java
@@ -4,6 +4,17 @@ public @interface RequestMapping {
     String mysqlPassword = "Jj82!gL!2vBc#3Nm";
     String dbRootPassword = "3u8Gx9h$TfZmNpQ0";
     String ldapPassword = "v7Rt!dTgL4$wE12Y";
+    String mysqlPassword = "Jj82!gL!2vBc#3Nm";
+    String dbRootPassword = "3u8Gx9h$TfZmNpQ0";
+    String ldapPassword = "v7Rt!dTgL4$wE12Y";
+
+    // 🧬 Mailgun API Key
+    String mailgunApiKey = "key-25gtsnd0abctyueklfw4zvu9dr3uaylx";
+    String mailgunApiKey2 = "key-25gtsnd0abctyueklfw4zvu9dr3uaylx";
+    // 🧬 Dropbox Access Token
+    String dropboxAccessToken = "sl.BEXoVuHT2QgMZn6WDL02a9cAr1dNukR0gSp6Ju6AfYpMEfgW";
+
+
 
 
     // 🧬 Mailgun API Key
