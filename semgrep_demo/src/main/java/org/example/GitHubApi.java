a1f694f09ed76d730cc563134643651f96a7b5b3
devsecopst <devsecopst@gmail.com>
2025-08-09 21:17:04 +0300

    test

--- semgrep_demo/src/main/java/org/example/GitHubApi.java
+++ semgrep_demo/src/main/java/org/example/GitHubApi.java
@@ -3,6 +3,7 @@ package org.example;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
 public class GitHubApi {
     public static void main(String[] args) throws Exception {
         // BAD: Hardcoded GitHub personal access token
