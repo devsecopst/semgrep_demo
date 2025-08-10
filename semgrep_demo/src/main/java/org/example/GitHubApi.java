dfc745e7dff945b627981699e7e369972d3549bd
devsecopst <devsecopst@gmail.com>
2025-08-10 16:17:30 +0300

    Test

--- semgrep_demo/src/main/java/org/example/GitHubApi.java
+++ semgrep_demo/src/main/java/org/example/GitHubApi.java
@@ -11,6 +11,18 @@ public class GitHubApi {
         conn.setRequestProperty("Authorization", "token " + token);
         int responseCode = conn.getResponseCode();
         System.out.println("Response Code: " + responseCode);
+        String awsAccessKeyId = "AKIA5NH7L4P3KJ2U9D3N";
+        String awsSecretKey = "N7k58Gv+WbqUVsRiZcKZpu7FmAIfs8WlYuU5U9XL";
+        // 🔐 GitHub Token
+
+        String githubToken = "ghp_D8fR2sY9ABQpGgW2S3xF4dVYZkNpWqz57TbR";
+        String githubToken44 = "ghp_D8fR2sY9ABQpGgW2S3xF4dVYZkNpWqz57TbR";
+        // 🔐 GitLab Token
+        String gitlabToken = "glpat-28eFqsNLmXKJxuPbaYBZ";
+        String gitlabToken2 = "glpat-28eFqsNLmXKJxuPbaYBZ";
+        // 🔐 Slack Bot Token
+        String slackBotToken = "xoxb-875392083274-987654321098-zD47HhU0LvIUGY6oDsP7hVJf";
+        // 🔐 Twilio Auth Token
     }
 
 
