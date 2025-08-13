6e3fbeb3535b1b3ff34c44c310e231c92a19bd16
devsecopst <devsecopst@gmail.com>
2025-08-13 23:35:08 +0300

    test

---
+++ semgrep_demo/src/main/java/org/example/secretsDemo2.java
@@ -0,0 +1,66 @@
+package org.example;
+
+public class secretsDemo2 {
+
+    // AWS
+    private static final String AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE";
+    private static final String AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+
+    // Google API
+    private static final String GOOGLE_API_KEY = "AIzaSyD-1234567890abcdefghijklmnopqrstuv";
+
+    // Azure
+    private static final String AZURE_STORAGE_KEY = "Eby8vdM02xNOcqFeqV1Ix0z3r7t4ZAzx2qkzNn==";
+
+    // Slack
+    private static final String SLACK_TOKEN = "xoxb-123456789012-1234567890123-abcdefghijklmnopqrstuv";
+
+    // GitHub
+    private static final String GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwx";
+
+    // Stripe
+    private static final String STRIPE_SECRET_KEY = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+
+    // Twilio
+    private static final String TWILIO_AUTH_TOKEN = "1234567890abcdef1234567890abcdef";
+
+    // Database
+    private static final String DB_PASSWORD = "P@ssw0rd1234!";
+
+    // JWT
+    private static final String JWT_SECRET = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+            + "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+    // Private Key (truncated)
+    private static final String PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\n"
+            + "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQD...\n"
+            + "-----END PRIVATE KEY-----";
+
+    // Facebook
+    private static final String FACEBOOK_APP_SECRET = "1a2b3c4d5e6f7g8h9i0j";
+
+    // Dropbox
+    private static final String DROPBOX_TOKEN = "sl.ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+    // DigitalOcean
+    private static final String DIGITALOCEAN_TOKEN = "dop_v1_1234567890abcdefghijklmnopqrstu";
+
+    // PayPal
+    private static final String PAYPAL_CLIENT_SECRET = "EAWCzO3p4a6h7g8f9e0d1c2b3a4x5y6z";
+
+    // Shopify
+    private static final String SHOPIFY_API_KEY = "shpat_1234567890abcdef1234567890abcdef";
+
+    // Heroku
+    private static final String HEROKU_API_KEY = "12345678-aaaa-bbbb-cccc-1234567890ab";
+
+    // SendGrid
+    private static final String SENDGRID_API_KEY = "SG.1234567890abcdefghijklmnopqrstuvwxyz";
+
+    // MongoDB URI
+    private static final String MONGODB_URI = "mongodb+srv://user:P@ssword123@cluster0.mongodb.net/mydb";
+
+    // Generic API Token
+    private static final String GENERIC_API_TOKEN = "token_1234567890abcdefghijklmnopqrstu";
+
+}
